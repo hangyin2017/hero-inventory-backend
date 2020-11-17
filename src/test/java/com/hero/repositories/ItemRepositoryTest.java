55b5ce1135c1b19f6334c3fceed913021215573c
Finnick <finnick9387@gmail.com>
2020-11-17 17:16:16 +1100

    update function deleteBrand check

--- src/test/java/com/hero/repositories/ItemRepositoryTest.java
+++ src/test/java/com/hero/repositories/ItemRepositoryTest.java
@@ -5,22 +5,24 @@ import com.hero.entities.Brand;
 import com.hero.entities.Item;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-/*@ExtendWith(SpringExtension.class)*/
-@SpringBootTest/*(classes = DemoApplication.class)*/
-@RunWith(SpringRunner.class)
-/*@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)*/
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = DemoApplication.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ItemRepositoryTest {
 
     @Autowired
     private ItemRepository itemRepository;
-/*
+
     @Test
     public void shouldInsertIntoDBGivenCorrectItemObject() {
         // Given
@@ -34,14 +36,14 @@ public class ItemRepositoryTest {
         // Then
         Assertions.assertEquals("beauty1", itemInDB.getName());
         Assertions.assertEquals("111", itemInDB.getCode());
-    }*/
-
-    @Test
-    public void test1(){
-        Brand brand = new Brand();
-        brand.setName("A2");
-        brand.setId(Long.parseLong("1"));
-        List<Item> list= itemRepository.findByBrand(brand);
-        System.out.println(list.size());
     }
+
+//    @Test
+//    public void test1(){
+//        Brand brand = new Brand();
+//        brand.setName("A2");
+//        brand.setId(Long.parseLong("1"));
+//        List<Item> list= itemRepository.findByBrand(brand);
+//        System.out.println(list.size());
+//    }
 }
