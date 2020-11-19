2ff8f1b48a270168a0de3c1100aaa243030ef5fb
finnick-666 <finnick9387@gmail.com>
2020-11-19 17:43:24 +1100

    create salesorders table in database by flyway

---
+++ src/main/resources/db/migration/V1_2__CREATE_SALESORDERS_TABLE.sql
@@ -0,0 +1,16 @@
+CREATE TABLE sales_orders (
+    salesorder_id SERIAL PRIMARY KEY,
+    salesorder_number VARCHAR(30),
+    reference_number VARCHAR(20),
+    date DATE,
+    status VARCHAR(10),
+    shipment_date DATE,
+--     contact_id INT REFERENCES contact (contact_id),
+    invoiced_status VARCHAR(30),
+    paid_status VARCHAR(30),
+    shipped_status VARCHAR(30),
+--     creator_id INT REFERENCES user (user_id),
+    created_time TIMESTAMP WITH TIME ZONE,
+    total_quality INT,
+    comments VARCHAR(255)
+);
