5e93850e20df0650f358009e566cebfa8ddaa2b0
finnick-666 <finnick9387@gmail.com>
2020-11-19 18:18:17 +1100

    create OrderItem and SalesOrder Entity

---
+++ src/main/java/com/hero/entities/SalesOrder.java
@@ -0,0 +1,55 @@
+package com.hero.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "sales_orders")
+public class SalesOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "salesorder_id")
+    private Long salesorderId;
+
+    @Column(name = "salesorder_number")
+    private String salesorderNumber;
+
+    @Column(name = "reference_number")
+    private String referenceNumber;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "shipment_date")
+    private Date shipmentDate;
+
+    @Column(name = "invoiced_status")
+    private String invoicedStatus;
+
+    @Column(name = "paid_status")
+    private String paidStatus;
+
+    @Column(name = "shipped_status")
+    private String shippedStatus;
+
+    @Column(name = "created_time")
+    private Date createdTime;
+
+    @Column(name = "total_quality")
+    private Long totalQuality;
+
+    @Column(name = "comments")
+    private String comments;
+
+    @OneToMany(mappedBy = "salesorder")
+    private Set<OrderedItem> orderedItems;
+}
