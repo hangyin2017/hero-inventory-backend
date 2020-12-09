6cb05ecc093755b9b54eca74a97a06a4967e7daf
finnick-666 <finnick9387@gmail.com>
2020-12-09 16:26:17 +1100

    fix customer DTOs

--- src/main/java/com/hero/dtos/customer/CustomerPutDto.java
+++ src/main/java/com/hero/dtos/customer/CustomerPutDto.java
@@ -1,9 +1,25 @@
 package com.hero.dtos.customer;
 
+import com.hero.entities.SalesOrder;
 import lombok.Data;
+
+import java.util.Date;
+import java.util.Set;
 
 @Data
 public class CustomerPutDto {
-    private Long customerId;
+    private Long id;
     private String customerName;
+    private String companyName;
+    private String website;
+    private Boolean active;
+    private String salutation;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Date createdTime;
+    private Date lastModifiedTime;
+    private String comments;
+    private Set<SalesOrder> salesOrders;
 }
--- src/main/java/com/hero/entities/Customer.java
+++ src/main/java/com/hero/entities/Customer.java
@@ -19,7 +19,7 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "customer_id")
-    private Long customerId;
+    private Long id;
 
     @Column(name = "customer_name")
     private String customerName;
