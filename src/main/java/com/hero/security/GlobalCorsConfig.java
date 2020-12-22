b74e873942de43965119c7ce6e492210d6671751
Hang <hang.yin2017@outlook.com>
2020-12-22 16:37:34 +1100

    [HERO-63] Enabled login CORS

---
+++ src/main/java/com/hero/security/GlobalCorsConfig.java
@@ -0,0 +1,29 @@
+package com.hero.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class GlobalCorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.setAllowCredentials(true);
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedHeader("*");
+        config.addExposedHeader("Authorization");
+
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(configSource);
+    }
+}
