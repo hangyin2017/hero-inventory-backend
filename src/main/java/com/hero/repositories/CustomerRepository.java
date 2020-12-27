2e9441f394450b7551109bfa099f0584c9558083
Dennis0804 <dennissally1314@gmail.com>
2020-12-27 19:01:36 +1100

    Fixed some merging problems

--- src/main/java/com/hero/repositories/CustomerRepository.java
+++ src/main/java/com/hero/repositories/CustomerRepository.java
@@ -2,6 +2,7 @@ package com.hero.repositories;
 
 import com.hero.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.List;
 public interface CustomerRepository extends JpaRepository <Customer, Long>{
     Customer findByName (String name);
 
-    List<Customer> findByNameLike(@RequestParam String name);
+    @Query("select customer from Customer customer where lower(customer.name) like :searchInput")
+    List<Customer> findByNameLike(@RequestParam String searchInput);
 }
