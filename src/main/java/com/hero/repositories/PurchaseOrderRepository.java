afcc93b512eb1cbfe77f13acaa5b3932e4109132
Hang <hang.yin2017@outlook.com>
2020-12-27 18:33:24 +1100

    [HERO-69] PurchaseOrdr total amount returns 0 by default

--- src/main/java/com/hero/repositories/PurchaseOrderRepository.java
+++ src/main/java/com/hero/repositories/PurchaseOrderRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long> {
-    @Query(value = "select sum(total_price) from purchaseorders",nativeQuery = true)
+    @Query(value = "select COALESCE(sum(total_price),0) from purchaseorders",nativeQuery = true)
     long getTotalPurchaseOrderPrice();
 }
